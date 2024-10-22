e3f419a3a322ab131777437044ab0318f4dbc37c
hoansharigan <nguyenthanhhoan300904@gmail.com>
2024-10-22 22:05:58 +0700

    ngay 2 ngay 3

--- src/main/java/com/example/cake/CakeApplication.java
+++ src/main/java/com/example/cake/CakeApplication.java
@@ -3,7 +3,12 @@ package com.example.cake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// tắt đi security 
+// include >< exclude bao gồm >< loại bỏ 
+
+// dòng code bên bên dưới có nghĩa là loại bỏ class security đi , khi truy cập vào trang
+// web thì không cần đăng nhập vào
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class CakeApplication {
 
 	public static void main(String[] args) {
