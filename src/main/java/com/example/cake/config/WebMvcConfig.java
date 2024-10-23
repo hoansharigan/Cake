7c27501b81f0d2d24d8d1f0cd45b84154acd5253
hoansharigan <nguyenthanhhoan300904@gmail.com>
2024-10-23 23:41:16 +0700

    Ngay 4

--- src/main/java/com/example/cake/config/WebMvcConfig.java
+++ src/main/java/com/example/cake/config/WebMvcConfig.java
@@ -1,19 +1,47 @@
 package com.example.cake.config;
 
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.ViewResolver;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.web.servlet.view.InternalResourceViewResolver;
+// import org.springframework.web.servlet.view.JstlView;
+
+// // cách thư 2  để config jsp 
+// @Configuration
+// @EnableWebMvc
+// public class WebMvcConfig implements WebMvcConfigurer {
+
+//   @Bean
+//   public ViewResolver viewResolver() {
+//     final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+//     bean.setViewClass(JstlView.class);
+//     bean.setPrefix("cake/WEB-INF/view/");
+//     bean.setSuffix(".jsp");
+//     return bean;
+//   }
+
+//   @Override
+//   public void configureViewResolvers(ViewResolverRegistry registry) {
+//     registry.viewResolver(viewResolver());
+//   }
+// }
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-// cách thư 2  để config jsp 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
-
   @Bean
   public ViewResolver viewResolver() {
     final InternalResourceViewResolver bean = new InternalResourceViewResolver();
@@ -27,6 +55,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void configureViewResolvers(ViewResolverRegistry registry) {
     registry.viewResolver(viewResolver());
   }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
+    registry.addResourceHandler("/js/**").addResourceLocations("/resources/js/");
+    registry.addResourceHandler("/images/**").addResourceLocations("/resources/images/");
+    registry.addResourceHandler("/client/**").addResourceLocations("/resources/client/");
+  }
 }
-
-
